3235921b254cd7ce97a9207ebdd7c7532ae35402
lokeshloki3 <lokesh1992p@gmail.com>
2024-10-21 18:36:13 +0530

    routes updated

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ function App() {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col">
         <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
 
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='/loginswitchtabsreact' element={<Home/>}/>
           <Route path='/login' element={<Login setIsLoggedIn={setIsLoggedIn}/>}/>
           <Route path='/signup' element={<Signup setIsLoggedIn={setIsLoggedIn}/>}/>
           <Route path="/dashboard" element = {
