8524433a0b4e6d61051f6ad490d0aa7232ed3f0d
lokeshloki3 <lokesh1992p@gmail.com>
2024-10-21 18:58:56 +0530

    ui updated

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,6 @@
 function Dashboard() {
   return (
-    <div className='flex justify-center items-center text-white text-3xl h-full'>
+    <div className='flex justify-center items-center text-white text-3xl min-h-screen'>
       Welcome to Studynotion!
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,6 @@ function Home() {
   return (
     <div className='flex flex-col justify-center items-center text-white text-3xl min-h-screen overflow-y-hidden'>
       Home
-      <Link to="/login">
-        <button className='bg-richblack-800 text-richblack-100 py-[8px] 
-        px-[12px] rounded-[8px] border border-richblack-700 mt-6'>Click to login</button>
-      </Link>
     </div>
   );
 }
